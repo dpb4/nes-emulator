e61ba635ff9c18e3ef3062b807709fded0f49b31
declanbbbbb <declan.bnbg@gmail.com>
2025-02-20 14:46:29 -0600

    tested comparison instructions

--- src/cpu/cpu/tests/instructions/test_compare_ins.rs
+++ src/cpu/cpu/tests/instructions/test_compare_ins.rs
@@ -1,1 +1,69 @@
 // CMP, CPX, CPY
+use crate::cpu::{cpu::tests::set_multiple_bytes, instructions, CPU};
+use instructions as IN;
+
+#[test]
+fn test_cmp() {
+    let mut cpu = CPU::new();
+    set_multiple_bytes(&mut cpu, 0, &vec![0, 5, 0, 20, 0, 10]);
+
+    cpu.accumulator = 10;
+    cpu.reg_x = 100;
+    cpu.reg_y = 200;
+
+    cpu.execute(IN::CMP_IM);
+
+    assert_eq!(cpu.flags, 0b00000001);
+
+    cpu.execute(IN::CMP_IM);
+
+    assert_eq!(cpu.flags, 0b10000000);
+
+    cpu.execute(IN::CMP_IM);
+
+    assert_eq!(cpu.flags, 0b00000011);
+}
+
+#[test]
+fn test_cpx() {
+    let mut cpu = CPU::new();
+    set_multiple_bytes(&mut cpu, 0, &vec![0, 5, 0, 20, 0, 10]);
+
+    cpu.accumulator = 100;
+    cpu.reg_x = 10;
+    cpu.reg_y = 200;
+
+    cpu.execute(IN::CPX_IM);
+
+    assert_eq!(cpu.flags, 0b00000001);
+
+    cpu.execute(IN::CPX_IM);
+
+    assert_eq!(cpu.flags, 0b10000000);
+
+    cpu.execute(IN::CPX_IM);
+
+    assert_eq!(cpu.flags, 0b00000011);
+}
+
+#[test]
+fn test_cpy() {
+    let mut cpu = CPU::new();
+    set_multiple_bytes(&mut cpu, 0, &vec![0, 5, 0, 20, 0, 10]);
+
+    cpu.accumulator = 100;
+    cpu.reg_x = 200;
+    cpu.reg_y = 10;
+
+    cpu.execute(IN::CPY_IM);
+
+    assert_eq!(cpu.flags, 0b00000001);
+
+    cpu.execute(IN::CPY_IM);
+
+    assert_eq!(cpu.flags, 0b10000000);
+
+    cpu.execute(IN::CPY_IM);
+
+    assert_eq!(cpu.flags, 0b00000011);
+}
--- src/cpu/cpu/tests/instructions/test_flag_ins.rs
+++ src/cpu/cpu/tests/instructions/test_flag_ins.rs
@@ -1,1 +1,52 @@
 // CLC, SEC, CLI, SEI, CLD, SED, CLV
+use crate::cpu::{instructions, Flag, CPU};
+use instructions as IN;
+
+#[test]
+fn test_clc() {
+    let mut cpu = CPU::new();
+    cpu.set_flag(Flag::Carry, 1);
+    cpu.set_flag(Flag::Overflow, 1);
+
+    cpu.execute(IN::CLC);
+
+    assert_eq!(cpu.get_flag(Flag::Carry), 0);
+    assert_eq!(cpu.get_flag(Flag::Overflow), 1);
+}
+
+#[test]
+fn test_cld() {
+    let mut cpu = CPU::new();
+    cpu.set_flag(Flag::Decimal, 1);
+    cpu.set_flag(Flag::Overflow, 1);
+
+    cpu.execute(IN::CLD);
+
+    assert_eq!(cpu.get_flag(Flag::Decimal), 0);
+    assert_eq!(cpu.get_flag(Flag::Overflow), 1);
+}
+
+#[test]
+fn test_cli() {
+    // TODO this one is special
+    let mut cpu = CPU::new();
+    cpu.set_flag(Flag::Interrupt, 1);
+    cpu.set_flag(Flag::Overflow, 1);
+
+    cpu.execute(IN::CLI);
+
+    assert_eq!(cpu.get_flag(Flag::Interrupt), 0);
+    assert_eq!(cpu.get_flag(Flag::Overflow), 1);
+}
+
+#[test]
+fn test_clv() {
+    let mut cpu = CPU::new();
+    cpu.set_flag(Flag::Overflow, 1);
+    cpu.set_flag(Flag::Carry, 1);
+
+    cpu.execute(IN::CLV);
+
+    assert_eq!(cpu.get_flag(Flag::Overflow), 0);
+    assert_eq!(cpu.get_flag(Flag::Carry), 1);
+}
